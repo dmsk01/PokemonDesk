4ff0f087e10dcc3bc7a1a478e23cadf755962017
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-05-10 20:39:10 +0300

    webpack config without node-sass

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./custom.css";
+import styles from "./App.modules.css";
+
+const App = () => {
+  return <div className={styles.header + " color"}>Hello world from App Component</div>;
+};
+
+export default App;
