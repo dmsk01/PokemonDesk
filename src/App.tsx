25e5c10f303e69b5b391cd427321f090cd6f3caf
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-05-12 18:21:50 +0300

    homework2

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from "react";
 
 import cn from "classnames";
 
-import "./custom.css";
-import styles from "./App.modules.scss";
+import styles from "./App.module.scss";
 
 const App = () => {
   return <div className={cn(styles.header, "color")}>Hello world from App Component</div>;
