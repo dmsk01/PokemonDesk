9f4c73223f52ece600c52a96aa062ef70d443ee3
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-05-12 19:03:03 +0300

    Added eslint && prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import cn from "classnames";
+import cn from 'classnames';
 
-import styles from "./App.module.scss";
+import styles from './App.module.scss';
 
 const App = () => {
-  return <div className={cn(styles.header, "color")}>Hello world from App Component</div>;
+  console.log('asfasfsafasdffads');
+
+  return <div className={cn(styles.header, 'color')}>Hello world from App Component</div>;
 };
 
 export default App;
