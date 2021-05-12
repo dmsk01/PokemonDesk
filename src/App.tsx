df7e5fbda7d6055bb28d50a80a7b8aef0014f029
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-05-12 23:34:40 +0300

    2 task fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import cn from 'classnames';
 import styles from './App.module.scss';
 
 const App = () => {
-  console.log('asfasfsafasdffads');
-
   return <div className={cn(styles.header, 'color')}>Hello world from App Component</div>;
 };
 
