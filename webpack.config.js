859790386e2c373856199c4fcd464575aa2c6f43
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-05-15 17:14:37 +0300

    hw3 html

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,60 @@
-const path = require("path");
-const HTMLWebpackPlugins = require("html-webpack-plugin");
+const path = require('path');
+const HTMLWebpackPlugins = require('html-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
   },
-  mode: NODE_ENV ? NODE_ENV : "development",
-  entry: path.resolve(__dirname, "src/index.ts"),
+  mode: NODE_ENV ? NODE_ENV : 'development',
+  entry: path.resolve(__dirname, 'src/index.ts'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000,
   },
   module: {
     rules: [
       {
         test: /\.[tj]sx?$/,
-        use: ["ts-loader"],
+        use: ['ts-loader'],
       },
       {
         test: /\.css$/,
-        exclude:/node_modules/,
-        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.scss$/,
         use: [
-          "style-loader",
-          "css-modules-typescript-loader?modules",
+          'style-loader',
+          'css-modules-typescript-loader?modules',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               modules: {
-                mode: "local",
-                localIdentName: "[name]__[local]__[hash:base64:5]",
+                mode: 'local',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
                 auto: /\.module\.\w+$/i,
               },
             },
           },
-          "sass-loader",
+          'sass-loader',
         ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
       },
     ],
   },
   plugins: [
     new HTMLWebpackPlugins({
-      template: path.resolve(__dirname, "public/index.html"),
+      template: path.resolve(__dirname, 'public/index.html'),
     }),
   ],
   devServer: {
@@ -53,5 +62,5 @@ module.exports = {
     open: true,
     hot: true,
   },
-  devtool: "source-map",
+  devtool: 'source-map',
 };
