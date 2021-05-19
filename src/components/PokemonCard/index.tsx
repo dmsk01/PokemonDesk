4d2c3627a351bb9aa7a1abdb184b0a8fe91b92e2
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-05-19 23:08:16 +0300

    pokemon card labels separated

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -5,8 +5,24 @@ import s from './PokemonCard.module.scss';
 
 interface IPokemonCardProps {
   [key: string]: string | number | string[];
+  types: string[];
   img: string;
+  name: string;
 }
+
+interface IColor {
+  [color: string]: string[];
+}
+const pokemonTypesColors: IColor = {
+  '#a1a1a1': ['stile', 'dark', 'rock'],
+  '#70a83b': ['grass', 'bug'],
+  '#a2cff0': ['ice', 'water'],
+  '#f76545': ['fire', 'fighting', 'dragon'],
+  '#76aadb': ['normal', 'gosth', 'flying'],
+  '#a974bc': ['poison', 'psychic', 'fairy', 'ghost'],
+  '#9b897b': ['ground'],
+  '#f7c545': ['electric'],
+};
 
 const PokemonCard: React.FC<IPokemonCardProps> = ({ name, attack, defence, types, img }) => {
   return (
@@ -26,11 +42,24 @@ const PokemonCard: React.FC<IPokemonCardProps> = ({ name, attack, defence, types
           </div>
         </div>
         <div className={s.labelWrap}>
-          <span className={s.label}>{types}</span>
+          {types.map((type) => {
+            let labelColor;
+            for (const [color, pokemonTypes] of Object.entries(pokemonTypesColors)) {
+              if (pokemonTypes.includes(type)) {
+                labelColor = color;
+                break;
+              }
+            }
+            return (
+              <span key={type} style={{ background: labelColor }} className={s.label}>
+                {type}
+              </span>
+            );
+          })}
         </div>
       </div>
       <div className={s.pictureWrap}>
-        <img src={img} alt="Charmander" />
+        <img src={img} alt={name} />
       </div>
     </div>
   );
