8d55dd1670d212fea7191d18b7347705a2d6c8d0
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-05-30 21:21:12 +0300

    hw7 jest config fix

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
+    '^.+\\.(css|less|scss|svg|png|jpeg|jpg)$': 'identity-obj-proxy',
   },
 };
