8a50a4d73a494a169327f59d9f940f748a334063
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-05-31 22:51:28 +0300

    server added

--- src/utils/toCapitalizeFirstLetter.test.ts
+++ src/utils/toCapitalizeFirstLetter.test.ts
@@ -2,9 +2,7 @@ import capitalizeFirstLetter from './toCapitalizeFirstLetter';
 
 describe('capitalize first letter function tests', () => {
   test('should receive string and return the same string with capital first letter', () => {
-    const string = capitalizeFirstLetter('string');
-
-    expect(string).toBe('String');
+    expect(capitalizeFirstLetter('string')).toBe('String');
   });
   test('should receive empty string and return 0', () => {
     expect(capitalizeFirstLetter('')).toBe(0);
