5aef91ffb8adabe0fb27707dfe063d7b4bc8bc62
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-06-23 21:43:09 +0300

    deploy version

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -66,7 +66,15 @@ module.exports = {
       },
       {
         test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        use: ['url-loader'],
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+            },
+          },
+        ],
       },
     ],
   },
